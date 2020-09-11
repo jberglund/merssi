9c6d6c2bafe074029301db755ed7351b7d38bf5a
Jacob <jacob@jberglund.se>
2020-09-11 08:45:06 +0200

    Changed strategy for queryStringParams splitting

--- src/lambda/get_rss.js
+++ src/lambda/get_rss.js
@@ -2,18 +2,21 @@ import { getRSS } from "./rss/index";
 import { formatRSS } from "./rss/format";
 
 exports.handler = async (event, context) => {
-  const queryStringUrl = event.queryStringParameters["url"];
+  const queryStringUrl = event.queryStringParameters["url"].split("|");
+  console.log(queryStringUrl);
+
   const rssUrls = Array.isArray(queryStringUrl)
     ? [...queryStringUrl]
     : [queryStringUrl];
 
-  console.log({
-    queryStringUrl,
-    rssUrls,
-    isArray: Array.isArray(queryStringUrl),
-  });
+  // console.log({
+  //   queryStringUrl,
+  //   rssUrls,
+  //   isArray: Array.isArray(queryStringUrl),
+  // });
+
   let feeds = await getRSS(rssUrls);
-  console.log(feeds);
+
   return {
     statusCode: 200,
     body: JSON.stringify(formatRSS(feeds)),
